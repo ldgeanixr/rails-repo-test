f63e2cbc9aa76e5d94fba794ec963e66082633fa
Akzhol <ldgeanixr@gmail.com>
2018-10-08 21:21:21 +0600

    install bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_142310) do
+ActiveRecord::Schema.define(version: 2018_10_01_035429) do
 
   create_table "articles", force: :cascade do |t|
-    t.string 	"title"
-    t.text 		"description"
-    t.datetime 	"created_at"
-    t.datetime 	"updated_at"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
